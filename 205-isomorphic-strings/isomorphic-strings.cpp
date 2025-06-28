1806170f6e1d8ca5116467261b7e6c8cbde61cc8
Devansh Mishra <115351387+dkmishra2407@users.noreply.github.com>
2025-06-28 21:34:04 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (80.55%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -2,17 +2,17 @@ class Solution {
 public:
     bool isIsomorphic(string s, string t) {
         if (s.length() != t.length()) return false;
-
-        unordered_map<char, char> m1, m2;
+        
+        char m1[256] = {0};
+        char m2[256] = {0};
 
         for (int i = 0; i < s.length(); i++) {
-            char a = s[i], b = t[i];
+            if(m1[s[i]] != m2[t[i]]){
+                return false;
+            }
 
-            if (m1.count(a) && m1[a] != b) return false;
-            if (m2.count(b) && m2[b] != a) return false;
-
-            m1[a] = b;
-            m2[b] = a;
+            m1[s[i]] = i+1;
+            m2[t[i]] = i+1;
         }
 
         return true;
