3174426d7f4ca8f6a7e1dbd9142fb315cec02aac
Devansh Mishra <115351387+dkmishra2407@users.noreply.github.com>
2025-07-01 21:10:19 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (29.69%) - LeetSync

--- 3617-find-the-original-typed-string-i/find-the-original-typed-string-i.cpp
+++ 3617-find-the-original-typed-string-i/find-the-original-typed-string-i.cpp
@@ -1,20 +1,6 @@
 class Solution {
 public:
     int possibleStringCount(string word) {
-        // unordered_map<char,int>m;
-
-        // for(auto it:word){
-        //     m[it]++;
-        // }
-        // int cnt = 0;
-        // for(auto it:m){
-        //     if(it.second > 1){
-        //         cnt += (it.second - 1);
-        //     }
-        // }
-
-        // return cnt+1;
-
         int cnt = 0;
         int n = word.length();
 
