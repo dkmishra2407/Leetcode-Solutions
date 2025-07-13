8d04046b7e76aacdfce48d7c1257f052084ec801
Devansh Mishra <115351387+dkmishra2407@users.noreply.github.com>
2025-07-13 11:24:11 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (64.11%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -17,7 +17,16 @@ public:
     int rob(vector<int>& nums) {
         int n = nums.size();
         vector<int>dp(n,-1);
+        dp[0] = nums[0];
+        for(int i=1;i<n;i++){
+            int pick = nums[i];
+            if(i > 1){
+                pick += dp[i-2];
+            }
+            int non_pick = dp[i-1];
+            dp[i] = max(pick,non_pick);
+        }
 
-        return f(nums,dp,0,n);
+        return dp[n-1];
     }
 };
