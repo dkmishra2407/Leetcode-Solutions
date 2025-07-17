cef1baf654b1a3035ffed5728022fec86fd5782f
Devansh Mishra <115351387+dkmishra2407@users.noreply.github.com>
2025-07-17 20:28:34 +0530

    Time: 0 ms (100.00%) | Memory: 14 MB (87.97%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,23 +1,18 @@
 class Solution {
 public:
-    int f(vector<int>& nums,int n,vector<vector<int>>&dp,int ind,int prev_ind){
-        if(ind == n){
-            return 0;
-        }
-        if(dp[ind][prev_ind+1]  != -1){
-            return dp[ind][prev_ind+1];
-        }
-        int len = 0 + f(nums,n,dp,ind+1,prev_ind);
-        if(prev_ind == -1 || nums[ind] > nums[prev_ind]){
-            len = max(len,1+f(nums,n,dp,ind+1,ind));
-        }
-
-        return dp[ind][prev_ind+1] = len;
-    }
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
-
-        return f(nums,n,dp,0,-1);
+        vector<int>hash;
+        hash.push_back(nums[0]);
+        for(int i=1;i<n;i++){
+            if(nums[i] > hash.back()){
+                hash.push_back(nums[i]);
+            }
+            else{
+                int ans = lower_bound(hash.begin(),hash.end(),nums[i]) - hash.begin();
+                hash[ans] = nums[i];
+            }
+        }
+        return hash.size();
     }
 };
